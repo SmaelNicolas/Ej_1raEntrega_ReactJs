1e87257c42069493f87e2e5ef857caeb1ff67e7c
Smael Nicolas <nico.smael@gmail.com>
2022-12-13 13:33:50 -0300

    import itemListcontainer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
 	return (
 		<div className='container--app'>
 			<Navbar />
+			<ItemListContainer />
 		</div>
 	);
 }
