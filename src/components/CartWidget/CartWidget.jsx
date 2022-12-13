536bdc457de7c8abbe11709e90ddf7dcf897f72d
Smael Nicolas <nico.smael@gmail.com>
2022-12-13 14:03:02 -0300

    add cart widget content

--- src/components/CartWidget/CartWidget.jsx
+++ src/components/CartWidget/CartWidget.jsx
@@ -1,5 +1,10 @@
 import "cartWidget.css";
 
 export const CartWidget = () => {
-	return <div>CartWidget</div>;
+	return (
+		<div className='container--cartWidget'>
+			<div className='cart--icon'>🛒</div>
+			<div className='cart--quantity'>5</div>
+		</div>
+	);
 };
