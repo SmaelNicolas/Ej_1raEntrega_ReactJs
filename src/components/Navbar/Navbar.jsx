288432cc64a98fd2d94b5525e7e14a765cc31c25
Smael Nicolas <nico.smael@gmail.com>
2022-12-13 14:01:55 -0300

    navbar content

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,29 @@
 import "./navbar.css";
 
 export const Navbar = () => {
-	return <div>Navbar</div>;
+	return (
+		<nav className='container--navbar'>
+			{/* LOGO */}
+			<div className='navbar--logo'>🪐</div>
+
+			{/* Secciones */}
+			<ul className='navbar--ul'>
+				<li className='navbar--ul--li'>
+					<a className='navbar--ul--li--a' href='#section1'>
+						Section 1
+					</a>
+				</li>
+				<li className='navbar--ul--li'>
+					<a className='navbar--ul--li--a' href='#section2'>
+						Section 2
+					</a>
+				</li>
+				<li className='navbar--ul--li'>
+					<a className='navbar--ul--li--a' href='#section3'>
+						Section 3
+					</a>
+				</li>
+			</ul>
+		</nav>
+	);
 };
