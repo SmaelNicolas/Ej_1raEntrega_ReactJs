01527b61ae6ea284a2c7dc369b9db7c4060913ad
Smael Nicolas <nico.smael@gmail.com>
2022-12-13 13:47:02 -0300

    add open in new tab

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -6,6 +6,8 @@ export const ItemListContainer = ({ greeting }) => {
 			<div className='container--ItemListContainer--text'>
 				Enviamos una
 				<a
+					target='_blank'
+					rel='noopener noreferrer'
 					className='itemListContainer--link'
 					href='https://beta.reactjs.org/learn/passing-props-to-a-component'>
 					PROP
@@ -15,15 +17,16 @@ export const ItemListContainer = ({ greeting }) => {
 			<div className='container--ItemListContainer--text'>
 				y la recibimos
 				<a
+					target='_blank'
+					rel='noopener noreferrer'
 					className='itemListContainer--link'
 					href='https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment'>
 					DESESTRUCTURADA
 				</a>
 			</div>
-			<div className='container--ItemListContainer--text'>
-				Prop ➡
-				<span className='itemListcontainer--prop'> {greeting} </span>
-			</div>
+			<div className='container--ItemListContainer--text'>Prop</div>
+			<div className='container--ItemListContainer--text'>⬇</div>
+			<div className='itemListcontainer--prop'> {greeting} </div>
 		</div>
 	);
 };
